58790b8d6ec7aa0e09c1e333270015ffccfd1da8
Erfan Ebrahimi <erfan.es1381@gmail.com>
2023-09-08 00:48:34 +0330

    Header, Transition
    add: header and transition

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,47 @@
+import Link from "next/link";
+import {
+  RiYoutubeLine,
+  RiInstagramLine,
+  RiFacebookBoxLine,
+  RiDribbbleLine,
+  RiPinterestLine,
+  RiGithubLine,
+  RiLinkedinLine,
+  RiTwitterLine
+} from "react-icons/ri";
+import {HiOutlineMail} from "react-icons/hi";
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      {/* <Link href={""} className="hover:text-accent transiotn-all duration-300">
+        <RiYoutubeLine />
+      </Link>
+      <Link href={""} className="hover:text-accent transiotn-all duration-300">
+        <RiInstagramLine />
+      </Link>
+      <Link href={""} className="hover:text-accent transiotn-all duration-300">
+        <RiFacebookBoxLine />
+      </Link>
+      <Link href={""} className="hover:text-accent transiotn-all duration-300">
+        <RiDribbbleLine />
+      </Link>
+      <Link href={""} className="hover:text-accent transiotn-all duration-300">
+        <RiPinterestLine />
+      </Link> */}
+      {/* <Link href={"erfan.es1381@gmail.com"} className="hover:text-accent transiotn-all duration-300">
+        <HiOutlineMail />
+      </Link> */}
+      <Link href={"https://github.com/er-ebrahimi"} className="hover:text-accent transiotn-all duration-300">
+        <RiGithubLine />
+      </Link>
+      <Link href={"https://linkedin.com/in/linkedin.com/in/erfan-ebrahimi-46ba79220"} className="hover:text-accent transiotn-all duration-300">
+        <RiLinkedinLine />
+      </Link>
+      <Link href={"https://twitter.com/erfanesebrahimi"} className="hover:text-accent transiotn-all duration-300">
+        <RiTwitterLine />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
--- pages/index.js
+++ pages/index.js
@@ -53,9 +53,9 @@ const Home = () => {
       {/* img */}
       <div className="w-[1200px] h-full absolute right-0 bottom-0 ">
         <div className=" bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0">
-          image
+          
         </div>
-        <div>Particles</div>
+        <ParticlesContainer/>
         <motion.div className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%] "
         variants={fadeIn("up", 0.5)}
         initial="hidden"
